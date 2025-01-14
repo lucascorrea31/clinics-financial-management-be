74af98d6039358e975f274a022059742bd62ae88
Lucas Correa <lucas.nhl@gmail.com>
2025-01-14 10:36:02 -0300

    fix logger

--- src/infrastructure/logging/logger.ts
+++ src/infrastructure/logging/logger.ts
@@ -3,7 +3,7 @@ import pino from "pino";
 const logger = pino({
 	level: process.env.LOG_LEVEL || "info",
 	transport: {
-		target: process.env.NODE_ENV === "production" ? "pino-pretty" : "stdout",
+		target: process.env.NODE_ENV === "production" ? "stdout" : "pino-pretty",
 		options: {
 			colorize: true,
 			translateTime: true,
