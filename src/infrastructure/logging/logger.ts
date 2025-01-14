707d555b2ec179c4e57738111ad8d4d35bf7cde6
Lucas Correa <lucas.nhl@gmail.com>
2025-01-14 10:33:30 -0300

    update logger

--- src/infrastructure/logging/logger.ts
+++ src/infrastructure/logging/logger.ts
@@ -3,7 +3,7 @@ import pino from "pino";
 const logger = pino({
 	level: process.env.LOG_LEVEL || "info",
 	transport: {
-		target: "pino-pretty",
+		target: process.env.NODE_ENV === "production" ? "pino-pretty" : "stdout",
 		options: {
 			colorize: true,
 			translateTime: true,
