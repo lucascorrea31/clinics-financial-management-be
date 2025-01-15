e5693f75b258e298330f41c6bc816576e36d3874
Lucas Correa <lucas.nhl@gmail.com>
2025-01-15 13:09:00 -0300

    change actions

--- src/infrastructure/logging/logger.ts
+++ src/infrastructure/logging/logger.ts
@@ -3,18 +3,18 @@ import pino from "pino";
 const isProduction = process.env.NODE_ENV === "production";
 
 const logger = pino({
-  level: process.env.LOG_LEVEL || "info",
-  ...(isProduction
-    ? {}
-    : {
-        transport: {
-          target: "pino-pretty",
-          options: {
-            colorize: true,
-            translateTime: true,
-          },
-        },
-      }),
+	level: process.env.LOG_LEVEL ?? "info",
+	...(isProduction
+		? {}
+		: {
+				transport: {
+					target: "pino-pretty",
+					options: {
+						colorize: true,
+						translateTime: true,
+					},
+				},
+			}),
 });
 
 export default logger;
--- src/main.ts
+++ src/main.ts
@@ -7,35 +7,35 @@ import logger from "./infrastructure/logging/logger";
 import morgan from "morgan";
 import { AppDataSource } from "./infrastructure/database/data-source";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 (async () => {
-  try {
-    logger.info("Starting server...");
+	try {
+		logger.info("Starting server...");
 
-    await new AppDataSource().setup();
-    logger.info("Database connected successfully.");
+		await new AppDataSource().setup();
+		logger.info("Database connected successfully.");
 
-    app.use(
-      morgan("combined", {
-        stream: {
-          write: (message: string) => logger.info(message.trim()),
-        },
-      }),
-    );
+		app.use(
+			morgan("combined", {
+				stream: {
+					write: (message: string) => logger.info(message.trim()),
+				},
+			})
+		);
 
-    // Use routes
-    app.use("/api", routes);
-    logger.info("API routes were added successfully.");
+		// Use routes
+		app.use("/api", routes);
+		logger.info("API routes were added successfully.");
 
-    // Start server
-    app.listen(PORT, () => {
-      logger.info(`Server running on http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    logger.error("Error during Data Source initialization:", error);
-    process.exit(1);
-  }
+		// Start server
+		app.listen(PORT, () => {
+			logger.info(`Server running on http://localhost:${PORT}`);
+		});
+	} catch (error) {
+		logger.error("Error during Data Source initialization:", error);
+		process.exit(1);
+	}
 })();
 
 export default app;
